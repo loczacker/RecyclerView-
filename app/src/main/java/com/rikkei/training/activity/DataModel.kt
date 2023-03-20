987c16a17371b6283cc580ec7fdf6580f4af4f25
loczacker <tienloctao@gmail.com>
2023-03-20 10:41:55 +0700

    Second Commit

---
+++ app/src/main/java/com/rikkei/training/activity/DataModel.kt
@@ -0,0 +1,3 @@
+package com.rikkei.training.activity
+
+data class DataModel (var title: String, var desc: String, var image: Int)
--- app/src/main/java/com/rikkei/training/activity/MainActivity.kt
+++ app/src/main/java/com/rikkei/training/activity/MainActivity.kt
@@ -1,28 +1,77 @@
 package com.rikkei.training.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
 class MainActivity : AppCompatActivity() {
+//RecycleView LinearLayoutManager
+//    private var layoutManager: RecyclerView.LayoutManager?= null
+//    private var adapter: RecyclerView.Adapter<RecycleAdapter.ViewHolder>?= null
 
-    private var layoutManager: RecyclerView.LayoutManager?= null
-    private var adapter: RecyclerView.Adapter<RecycleAdapter.ViewHolder>?= null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var photoAdapter: PhotoAdapter
+    private var dataList = mutableListOf<DataModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        layoutManager = LinearLayoutManager(this)
 
-        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        //LinearLayoutManager
+//        layoutManager = LinearLayoutManager(this)
+//
+//        var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+//
+//        recyclerView.layoutManager = layoutManager
+//
+//        adapter = RecycleAdapter()
+//        recyclerView.adapter = adapter
 
-        recyclerView.layoutManager = layoutManager
 
-        adapter = RecycleAdapter()
-        recyclerView.adapter = adapter
+        //GridLayoutManager
+//        recyclerView = findViewById(R.id.recyclerView)
+//        recyclerView.layoutManager = GridLayoutManager(applicationContext, 2)
+//        photoAdapter = PhotoAdapter(applicationContext)
+//        recyclerView.adapter = photoAdapter
+//
+//        dataList.add(DataModel("Title", "Desc", R.drawable.three))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.four))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.five))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.six))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.jason))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.max))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.filip))
+//        dataList.add(DataModel("Title", "Desc", R.drawable.fernando))
+//
+//        photoAdapter.setDataList(dataList)
+
+
+        //StaggeredGridManager
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        photoAdapter = PhotoAdapter(applicationContext)
+        recyclerView.adapter = photoAdapter
+
+        dataList.add(DataModel("Title", "Desc", R.drawable.three))
+        dataList.add(DataModel("Title", "Desc", R.drawable.four))
+        dataList.add(DataModel("Title", "Desc", R.drawable.five))
+        dataList.add(DataModel("Title", "Desc", R.drawable.six))
+        dataList.add(DataModel("Title", "Desc", R.drawable.jason))
+        dataList.add(DataModel("Title", "Desc", R.drawable.max))
+        dataList.add(DataModel("Title", "Desc", R.drawable.filip))
+        dataList.add(DataModel("Title", "Desc", R.drawable.fernando))
+        dataList.add(DataModel("Title", "Desc", R.drawable.three))
+        dataList.add(DataModel("Title", "Desc", R.drawable.four))
+        dataList.add(DataModel("Title", "Desc", R.drawable.five))
+        dataList.add(DataModel("Title", "Desc", R.drawable.six))
+        dataList.add(DataModel("Title", "Desc", R.drawable.jason))
+        dataList.add(DataModel("Title", "Desc", R.drawable.max))
+        dataList.add(DataModel("Title", "Desc", R.drawable.filip))
+        dataList.add(DataModel("Title", "Desc", R.drawable.fernando))
+
+        photoAdapter.setDataList(dataList)
 
     }
 }
